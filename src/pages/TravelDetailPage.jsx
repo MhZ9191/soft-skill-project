32a1501e2c81b9cfc2281a4c91e1ad244eeed4ee
Salvo-Raia <salvo.raia@outlook.it>
2026-03-23 19:04:16 +0100

    Add: minor style changes

--- src/pages/TravelDetailPage.jsx
+++ src/pages/TravelDetailPage.jsx
@@ -4,10 +4,13 @@ import { Link } from "react-router";
 import { travels } from "../data/data";
 
 export default function TravelDetailPage() {
+  // Funzione per formattare la data in stile italiano
+  const formatDate = (dateString) => {
+    return new Date(dateString).toLocaleDateString("it-IT");
+  };
+
   const { id } = useParams();
-
   const travelDetail = travels.find((travel) => travel.id === Number(id));
-
   if (!travelDetail) return <p>Viaggio non trovato!</p>;
 
   return (
@@ -15,7 +18,7 @@ export default function TravelDetailPage() {
       <h1 className="fw-bold text-primary">I dettagli del tuo viaggio</h1>
 
       <div
-        className="travel-title my-4 p-4 text-light"
+        className="travel-jumbo my-4 p-4 text-light border rounded-4"
         style={{
           minHeight: "300px",
           textShadow: "0 1px 3px rgba(0,0,0,0.9), 0 4px 12px rgba(0,0,0,0.6)",
@@ -24,11 +27,14 @@ export default function TravelDetailPage() {
           backgroundPosition: "center",
         }}
       >
-        <h2>{travelDetail.to}</h2>
-        <p className="text-light fs-5">
-          <i className="bi bi-calendar-event me-2"></i>
-          Dal: {travelDetail.start} al: {travelDetail.end}
-        </p>
+        <div className="travel-jumbo-detail">
+          <h2>{travelDetail.to}</h2>
+          <p className="fs-6">
+            <i className="bi bi-calendar-event me-2"></i>
+            {formatDate(travelDetail.start)} - {formatDate(travelDetail.end)}
+          </p>
+          <p className="fs-4 ">Prezzo: {travelDetail.price}€</p>
+        </div>
       </div>
 
       {/* ROW con gap */}
@@ -40,8 +46,8 @@ export default function TravelDetailPage() {
               <i className="bi bi-geo-alt me-2"></i>
               Il tuo soggiorno
             </h4>
-
-            <h6 className="card-subtitle mb-2">{travelDetail.hotel}</h6>
+            {/* Valutiamo logo hotel + hotel rating */}
+            <h5 className="card-subtitle mb-2">{travelDetail.hotel}</h5>
 
             <p className="card-text">
               QUI POTREMMO INSERIRE UNA BREVE DESCRIZIONE DELL'HOTEL
@@ -61,8 +67,8 @@ export default function TravelDetailPage() {
               Il tuo accompagnatore
             </h4>
 
-            <h6 className="card-subtitle mb-2">{travelDetail.companion}</h6>
-
+            <h5 className="card-subtitle mb-2">{travelDetail.companion}</h5>
+            {/* Valutiamo propic accompagnatore */}
             <p className="card-text">
               QUI POTREMMO INSERIRE UNA BREVE DESCRIZIONE DELL'ACCOMPAGNATORE
             </p>
@@ -82,9 +88,9 @@ export default function TravelDetailPage() {
             Il tuo gruppo
           </h5>
 
-          <h6 className="card-subtitle mb-2">
+          <h5 className="card-subtitle mb-2">
             <b>Numero partecipanti:</b> {travelDetail.travelers.length}
-          </h6>
+          </h5>
 
           <p className="card-text">
             <b>Partecipanti:</b> {travelDetail.travelers.join(", ")}
