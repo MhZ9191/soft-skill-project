40cb5008bdf861d9e27d07e24835e1ff27d73df7
AntonioCappellano <antoniocappellano97@gmail.com>
2026-03-23 16:36:27 +0100

    add navbar

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,12 +1,33 @@
-import { Outlet } from "react-router";
+import { Link, Outlet } from "react-router";
+
+// creo la navbar per viaggiare tra le pagine
 
 export default function DefaultLayout() {
   return (
     <>
-      <header>
-        <h1>HEADER</h1>
-      </header>
-      <Outlet />
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <div className="navbar-nav">
+            <Link className="nav-link" aria-current="page" to="/">
+              Home
+            </Link>
+            <Link className="nav-link" aria-current="page" to="/TravelPage">
+              Viaggi
+            </Link>
+            <Link
+              className="nav-link"
+              aria-current="page"
+              to="/AddressBookPage"
+            >
+              Rubrica
+            </Link>
+          </div>
+        </div>
+      </nav>
+      
+      <main className="container mt-4">
+        <Outlet />
+      </main>
     </>
   );
 }
