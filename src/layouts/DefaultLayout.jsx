d46fde7fa8ee0cb1015c63e1b8d94478b2070348
AntonioCappellano <antoniocappellano97@gmail.com>
2026-03-24 11:23:15 +0100

    add link on site name

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -7,7 +7,7 @@ export default function DefaultLayout() {
     <>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
-          <span className="text-primary navbar-brand fs-3 fw-semibold">BooRoad</span>
+          <Link to="/" className="text-primary navbar-brand fs-3 fw-semibold">BooRoad</Link>
           <div className="navbar-nav ms-auto d-flex">
             <Link className="nav-link fs-4 fw-medium" aria-current="page" to="/">
               Home
